acf9bc37cbe6b4fc80ca68204cbf9e6c79009d53
unknown <habibrizky.2021@student.uny.ac.id>
2026-03-17 20:21:55 +0700

    menghilangkan comen

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'class', // <--- Tambahkan baris ini di sini
+    darkMode: 'class', 
 
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
